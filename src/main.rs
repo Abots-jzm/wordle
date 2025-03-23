42e014b5b643bb2a77c293b14bc5ee2a7286ef83
Tobi Abolarin <abolarinoluwatobistephen@gmail.com>
2025-03-23 00:03:09 -0500

    compute average

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,24 @@ use wordle::{solver::Solver, Wordle};
 
 fn main() {
     let wordle = Wordle::new();
+    let mut total_score = 0;
+    let mut games_played = 0;
+
     for answer in GAMES.split_whitespace() {
         let guesser = Solver::new();
         if let Some(score) = wordle.play(answer, guesser) {
             println!("guessed '{}' in {} tries\n", answer, score);
+            total_score += score;
+            games_played += 1;
         } else {
             eprintln!("failed to guess");
         }
     }
 
-    println!("Hello, world!");
+    if games_played > 0 {
+        let average = total_score as f64 / games_played as f64;
+        println!("Average score: {:.2} tries per word", average);
+    } else {
+        println!("No successful games played");
+    }
 }
