96f53cf4260d98504f2d1fd32489c19f77544ce4
Tobi Abolarin <abolarinoluwatobistephen@gmail.com>
2025-03-22 23:40:30 -0500

    don't search unlikely candidates

--- src/solver.rs
+++ src/solver.rs
@@ -18,13 +18,15 @@ impl Solver {
     pub fn new() -> Self {
         Self {
             remaining: Cow::Borrowed(INITIAL.get_or_init(|| {
-                Vec::from_iter(DICTIONARY.lines().map(|line| {
+                let mut words = Vec::from_iter(DICTIONARY.lines().map(|line| {
                     let (word, count) = line
                         .split_once(' ')
                         .expect("Every line is word + space + frequency");
                     let count: usize = count.parse().expect("every count is a number");
                     return (word, count);
-                }))
+                }));
+                words.sort_unstable_by_key(|&(_, count)| std::cmp::Reverse(count));
+                words
             })),
             patterns: Cow::Borrowed(PATTERNS.get_or_init(|| Correctness::patterns().collect())),
         }
@@ -64,6 +66,9 @@ impl Guesser for Solver {
 
         let remaining_count: usize = self.remaining.iter().map(|(_, c)| c).sum();
         let mut best: Option<Candidate> = None;
+
+        let mut i = 0;
+        let stop = (self.remaining.len() / 2).max(20);
         for &(word, count) in &*self.remaining {
             let mut sum = 0.0;
             let check_pattern = |pattern: &[Correctness; 5]| {
@@ -106,6 +111,11 @@ impl Guesser for Solver {
                 }
             } else {
                 best = Some(Candidate { word, goodness })
+            }
+
+            i += 1;
+            if i >= stop {
+                break;
             }
         }
         best.unwrap().word.to_string()
